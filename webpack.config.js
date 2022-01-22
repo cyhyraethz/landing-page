12d73d80dddb1d97e7eeed8d610578ae5cf25bda
Dylan Premo <cyhyraethz@gmail.com>
2022-01-22 01:11:51 -0800

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './public',
+    client: {
+      overlay: false,
+    },
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
